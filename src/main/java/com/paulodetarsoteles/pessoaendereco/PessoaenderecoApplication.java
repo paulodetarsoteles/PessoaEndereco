ea684bedb7f750db8cda83ec8a8b4099dcffbca2
Paulo de Tarso <potassio@hotmail.com>
2023-04-06 22:40:43 -0300

    Commit inicial, criação do projeto

---
+++ src/main/java/com/paulodetarsoteles/pessoaendereco/PessoaenderecoApplication.java
@@ -0,0 +1,13 @@
+package com.paulodetarsoteles.pessoaendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaenderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaenderecoApplication.class, args);
+	}
+
+}
